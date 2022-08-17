b90a031728c5721958e694c4ef7ed4ddc38e7e86
Artur Ardai <krkerdo@gmail.com>
2022-08-17 12:55:46 +0300

    yes

---
+++ AverageSalaryExcludingtheMinimumandMaximumSalary.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class AverageSalaryExcludingtheMinimumandMaximumSalary {
+    public double average(int[] salary) {
+        double sum=0;
+       Arrays.sort(salary);
+       for (int i = 1; i < salary.length-1; i++) {
+           sum+=salary[i];
+       }
+       return sum/(salary.length-2);
+   }
+}
