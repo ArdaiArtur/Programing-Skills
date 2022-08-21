b2c8302b3e7ea427c9ad3cb18ad313ab29f9cfd7
Artur Ardai <krkerdo@gmail.com>
2022-08-21 13:40:12 +0300

    s

---
+++ Day3/LargestPrimeterTriangle.java
@@ -0,0 +1,15 @@
+package Day3;
+
+import java.util.Arrays;
+
+public class LargestPrimeterTriangle {
+    public int largestPerimeter(int[] nums) {
+       Arrays.sort(nums);
+       
+       for (int i = nums.length - 3; i >= 0; --i)
+       if (nums[i] + nums[i+1] > nums[i+2])
+           return nums[i] + nums[i+1] + nums[i+2];
+   return 0;
+    }
+    
+}
