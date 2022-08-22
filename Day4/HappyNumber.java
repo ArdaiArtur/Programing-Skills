954954606917f5d591f1d17199aa15acb7451d20
Artur Ardai <krkerdo@gmail.com>
2022-08-22 16:37:30 +0300

    hear and tortes

--- Day4/HappyNumber.java
+++ Day4/HappyNumber.java
@@ -1,7 +1,24 @@
 package Day4;
 
 public class HappyNumber {
+    
+    public int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
     public boolean isHappy(int n) {
-        return true;
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+        while (fastRunner != 1 && slowRunner != fastRunner) {
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(fastRunner));
+        }
+        return fastRunner == 1;
     }
 }
