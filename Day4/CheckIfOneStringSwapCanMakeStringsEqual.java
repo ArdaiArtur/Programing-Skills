c0d499465b492c1f986c8bd7a573d7914cc8e4be
Artur Ardai <krkerdo@gmail.com>
2022-08-22 16:53:50 +0300

    ye

--- Day4/CheckIfOneStringSwapCanMakeStringsEqual.java
+++ Day4/CheckIfOneStringSwapCanMakeStringsEqual.java
@@ -2,18 +2,21 @@ package Day4;
 
 public class CheckIfOneStringSwapCanMakeStringsEqual {
     public boolean areAlmostEqual(String s1, String s2) {
+        
         int count=0;
         int[]ar=new int [26];
+        int []br=new int[26];
         if(s1.length()!=s2.length()) return false;
         for (int i = 0; i < s1.length(); i++) {
-            if(s1.indexOf(i)!=s2.indexOf(i)) count++;
+            if(s1.charAt(i)!=s2.charAt(i)) count++;
             if(count>=3) return false;
-            ar[s1.indexOf(i)-'a']++;
-            ar[s2.indexOf(i)-'a']++;
+            ar[s1.charAt(i)-'a']++;
+            br[s2.charAt(i)-'a']++;
         }
         for (int i = 0; i < ar.length; i++) {
-            if(ar[i]%2!=0) return false; 
+            if(br[i]!=ar[i]) return false;
         }
         return true;
+        
     }
 }
