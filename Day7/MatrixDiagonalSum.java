faf0d7c3c646ce5db2a089015263f35c5585210d
Artur Ardai <krkerdo@gmail.com>
2022-08-30 15:54:44 +0300

    bruh dudu sol

---
+++ Day7/MatrixDiagonalSum.java
@@ -0,0 +1,23 @@
+package Day7;
+
+public class MatrixDiagonalSum {
+    public int diagonalSum(int[][] mat) {
+       
+        int sum=0;
+        int n=mat.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i==j)
+                sum+=mat[i][j];
+                if(i+j==n-1) 
+                sum+=mat[i][j];
+
+                if(i==j&&i+j==n-1)
+                sum-=mat[i][j];
+                
+            }
+        }
+        return sum;
+        
+    }
+}
