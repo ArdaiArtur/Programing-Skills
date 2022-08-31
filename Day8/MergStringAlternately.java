9a5e51bd2cf259e4fd91ffc87fe90124ead5089a
Artur Ardai <krkerdo@gmail.com>
2022-08-31 15:50:34 +0300

    oke

---
+++ Day8/MergStringAlternately.java
@@ -0,0 +1,16 @@
+package Day8;
+
+public class MergStringAlternately {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb=new StringBuilder();
+        int n=word1.length();
+        int m=word2.length();
+        for (int i = 0; i < n+m; i++) {
+            if(i<n)
+            sb.append(word1.charAt(i));
+            if(i<m)
+            sb.append(word2.charAt(i));
+        }
+        return sb.toString();
+    }
+}
