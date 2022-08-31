6a778db173fd3d50e7caa1845b061dda44a3bd55
Artur Ardai <krkerdo@gmail.com>
2022-08-31 15:17:07 +0300

    ok

--- Day8/FindTheDifference.java
+++ Day8/FindTheDifference.java
@@ -1,5 +1,17 @@
 package Day8;
 
 public class FindTheDifference {
-    
+    public char findTheDifference(String s, String t) {
+        int []cnt=new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            cnt[s.charAt(i)-'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if(cnt[t.charAt(i)-'a']==0)
+            return t.charAt(i);
+            cnt[t.charAt(i)-'a']--;
+        }
+        return ' ';
+    }
 }
