b785aa9cf91032816ba971da5789f5f594a6e9cf
Artur Ardai <krkerdo@gmail.com>
2022-09-01 13:14:14 +0300

    ye

---
+++ Day9/ToLowerCase.java
@@ -0,0 +1,11 @@
+package Day9;
+
+public class ToLowerCase {
+    public String toLowerCase(String s) {
+        //97
+        //65
+        //-32
+        return s.toLowerCase();
+        
+    }
+}
