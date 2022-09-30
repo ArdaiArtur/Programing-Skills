4cea66914086847381c42b62f836f8f8f1570cb6
Artur Ardai <krkerdo@gmail.com>
2022-09-30 13:00:34 +0300

    ye v2

--- Day12/DesignParkingSystem.java
+++ Day12/DesignParkingSystem.java
@@ -1,16 +1,33 @@
 package Day12;
 
+import java.util.HashMap;
+
 public class DesignParkingSystem {
     int big;
     int medium;
     int small;
+    HashMap<Integer,Integer>hashMap=new HashMap<>();
     public DesignParkingSystem(int big, int medium, int small) {
-        this.big=big;
+        /* this.big=big;
         this.medium=medium;
-        this.small=small;
+        this.small=small; */
+        hashMap.put(1, big);
+        hashMap.put(2, medium);
+        hashMap.put(3, small);
+    }
+    public boolean addCar(int carType) {
+    if(hashMap.get(carType)>0)
+    {
+        hashMap.put(carType, hashMap.get(carType)-1);
+        return true;
     }
     
-    public boolean addCar(int carType) {
+    return false;
+    }
+
+
+
+    public boolean addCarv1(int carType) {
         if(carType==3)
         {
             if(small>0)
