a10b8df1a1cf0e405295b6601e0647af3e038a7d
Artur Ardai <krkerdo@gmail.com>
2022-10-26 20:18:11 +0300

    no

---
+++ Day2II/RepetedSubstringPattern.java
@@ -0,0 +1,10 @@
+package Day2II;
+
+import java.util.HashMap;
+
+public class RepetedSubstringPattern {
+    public boolean repeatedSubstringPattern(String s) {
+        if(s.length()==1) return false;
+       
+    }
+}
