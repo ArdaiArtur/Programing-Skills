d08b3ca6fad7a166603ed13c74ebf133efc37b90
Artur Ardai <krkerdo@gmail.com>
2022-10-27 16:42:09 +0300

    no but understand

--- Day2II/RepetedSubstringPattern.java
+++ Day2II/RepetedSubstringPattern.java
@@ -1,10 +1,22 @@
 package Day2II;
 
-import java.util.HashMap;
+
 
 public class RepetedSubstringPattern {
     public boolean repeatedSubstringPattern(String s) {
-        if(s.length()==1) return false;
-       
+        int len = s.length();
+    	for(int i=len/2 ; i>=1 ; i--) {
+    		if(len%i == 0) {
+    			int m = len/i;
+    			String subS = s.substring(0,i);
+    			int j;
+    			for(j=1;j<m;j++) {
+    				if(!subS.equals(s.substring(j*i,i+j*i))) break;
+    			}
+    			if(j==m)
+    			    return true;
+    		}
+    	}
+    	return false;
     }
 }
