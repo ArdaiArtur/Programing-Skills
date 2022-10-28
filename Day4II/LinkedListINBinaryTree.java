1900c61ddece73537b9ea18866f814dfe6695e9f
Artur Ardai <krkerdo@gmail.com>
2022-10-28 17:56:04 +0300

    yes

--- Day4II/LinkedListINBinaryTree.java
+++ Day4II/LinkedListINBinaryTree.java
@@ -2,8 +2,25 @@ package Day4II;
 
 public class LinkedListINBinaryTree {
     public boolean isSubPath(ListNode head, TreeNode root) {
-        
+        boolean ans=ret(head, root);
+        if(root!=null&&!ans)
+        {
+            ans|=isSubPath(head, root.left);
+            ans|=isSubPath(head, root.right);
+        }
+        return ans;
     }
+    public boolean ret(ListNode head,TreeNode root)
+    {
+        if(head==null)
+        return true;
 
+        if(root==null)
+        return false;
+
+        if(root.val==head.val)
+        return ret(head.next, root.left)|| ret(head.next, root.right);
+        return false;
+    }
     
 }
